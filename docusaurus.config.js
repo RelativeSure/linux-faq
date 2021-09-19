b37fe945b3f11008399690e3874607ce3b34eb40
Rasmus Brøgger Jørgensen <67839982+RelativeSure@users.noreply.github.com>
2021-09-19 11:24:45 +0200

    Create docusaurus.config.js

---
+++ docusaurus.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // ...
+  url: 'https://relativesure-linux.github.io', // Your website URL
+  baseUrl: '/',
+  projectName: 'relativesure-linux.github.io',
+  organizationName: 'relativesure-linux',
+  trailingSlash: false,
+  // ...
+};
